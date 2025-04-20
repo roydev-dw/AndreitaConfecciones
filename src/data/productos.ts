01262c8a1b5ea0c9f635ac15271a10bd2c91d664
roydev-dw <ado.pezzini@gmail.com>
2025-04-19 22:45:43 -0400

    se agrega contacto y se ajustan parametros de email

--- src/data/productos.ts
+++ src/data/productos.ts
@@ -25,4 +25,44 @@ export const productos: Producto[] = [
     descripcion: "Blusa elegante ideal para ocasiones especiales.",
     imagen: "/producto4-removebg-preview.png",
   },
+  {
+    titulo: "Polerón Rosado",
+    descripcion: "Polerón suave con diseño exclusivo.",
+    imagen: "/producto1-removebg-preview.png",
+  },
+  {
+    titulo: "Vestido Floral",
+    descripcion: "Vestido de verano con estampado floral.",
+    imagen: "/producto2-removebg-preview.png",
+  },
+  {
+    titulo: "Chaqueta Negra",
+    descripcion: "Chaqueta clásica para media estación.",
+    imagen: "/producto3-removebg-preview.png",
+  },
+  {
+    titulo: "Blusa Celeste",
+    descripcion: "Blusa elegante ideal para ocasiones especiales.",
+    imagen: "/producto4-removebg-preview.png",
+  },
+  {
+    titulo: "Polerón Rosado",
+    descripcion: "Polerón suave con diseño exclusivo.",
+    imagen: "/producto1-removebg-preview.png",
+  },
+  {
+    titulo: "Vestido Floral",
+    descripcion: "Vestido de verano con estampado floral.",
+    imagen: "/producto2-removebg-preview.png",
+  },
+  {
+    titulo: "Chaqueta Negra",
+    descripcion: "Chaqueta clásica para media estación.",
+    imagen: "/producto3-removebg-preview.png",
+  },
+  {
+    titulo: "Blusa Celeste",
+    descripcion: "Blusa elegante ideal para ocasiones especiales.",
+    imagen: "/producto4-removebg-preview.png",
+  },
 ];
