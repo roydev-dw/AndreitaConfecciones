a9e31d3ca5a76c09168e45adec547993025149fe
roydev-dw <ado.pezzini@gmail.com>
2025-05-04 23:25:04 -0400

    ajustes menu hamburguesa

--- src/scripts/init-embla.js
+++
@@ -1,8 +0,0 @@
-import emblaCarousel from "embla-carousel";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const emblaNode = document.querySelector(".embla");
-  if (emblaNode instanceof HTMLElement) {
-    emblaCarousel(emblaNode, { loop: true });
-  }
-});
